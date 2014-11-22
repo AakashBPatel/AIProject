1322c3508ea09a90281978b1504c8d5e48b65165
Aakash Patel <contact@aaka.sh>
2014-11-22 16:15:48 -0500

    Program now requires a filepath as an arg and will then output height then width then 0's or 1s

--- ShapeClassifier/src/ImageReader.java
+++ ShapeClassifier/src/ImageReader.java
@@ -9,32 +9,49 @@ import java.io.File;
 
 import javax.imageio.ImageIO;
 class ImageReader {
-   BufferedImage  image;
-   int width;
-   int height;
-   int data[][];
-   public ImageReader(String filename) {
-      try {
-         File input = new File(filename);
-         image = ImageIO.read(input);
-         width = image.getWidth();
-         height = image.getHeight();
-         data = new int[height][width];
-         int count = 0;
-         for(int i=0; i<height; i++){
-            for(int j=0; j<width; j++){
-               count++;
-               Color c = new Color(image.getRGB(j, i));
-              // System.out.println("S.No: " + count + " Red: " + c.getRed() +
-              // " Green: " + c.getGreen() + " Blue: " + c.getBlue());
-               data[i][j] = (c.getBlue() + c.getRed() + c.getGreen()) > 700 ? 0:1;
-               System.out.println("S. No: " + count + " Value: " + data[i][j]);
-               }
-            }
-      } catch (Exception e) {}
-   }
-   
-   public int[][] getData(){
-	   return data;
-   }
+	BufferedImage  image;
+	private int width;
+	private int height;
+	private int data[][];
+	public ImageReader(String filename){
+		try {
+			File input = new File(filename);
+			image = ImageIO.read(input);
+			width = image.getWidth();
+			height = image.getHeight();
+			data = new int[height][width];
+			int count = 0;
+			for(int i=0; i<height; i++){
+				for(int j=0; j<width; j++){
+					count++;
+					Color c = new Color(image.getRGB(j, i));
+					// System.out.println("S.No: " + count + " Red: " + c.getRed() +
+					// " Green: " + c.getGreen() + " Blue: " + c.getBlue());
+					data[i][j] = (c.getBlue() + c.getRed() + c.getGreen()) > 700 ? 0:1;
+					//System.out.println("S. No: " + count + " Value: " + data[i][j]);
+				}
+			}
+		} catch (Exception e) {}
+	}
+
+	public int[][] getData(){
+		return data;
+	}
+	public int getWidth(){
+		return width;
+	}
+	public int getHeight(){
+		return height;
+	}
+	
+	public static void main(String args[]){
+		ImageReader image = new ImageReader(args[0]);
+		System.out.println(image.getHeight());
+		System.out.println(image.getWidth());
+		for(int i = 0; i<image.getHeight(); i++){
+			for(int j = 0; j < image.getWidth(); j++)
+				System.out.println(image.getData()[i][j]);
+		}
+	}
 }
+
