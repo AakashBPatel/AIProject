5104e3ec5bf5c590b34e90ee1ba3c20a56ed501b
Aakash Patel <contact@aaka.sh>
2014-12-03 14:57:02 -0500

    removed excess prints

--- ShapeClassifier.py
+++ ShapeClassifier.py
@@ -2,7 +2,7 @@ import neural
 import shlex, subprocess
 
 def read_image(filepath):
-	cmd = '/Library/Java/JavaVirtualMachines/jdk1.8.0_20.jdk/Contents/Home/bin/java  -Dfile.encoding=UTF-8 -classpath "/Users/aakash/Sync/Artificial Intelligence/AIProject/ShapeClassifier/bin" ImageReader ' + filepath
+	cmd = 'java -Dfile.encoding=UTF-8 -classpath "/Users/aakash/Sync/Artificial Intelligence/AIProject/ShapeClassifier/bin" ImageReader ' + filepath
 	#print(cmd)
 	args = shlex.split(cmd)
 	output,error = subprocess.Popen(args,stdout = subprocess.PIPE, stderr= subprocess.PIPE).communicate()
@@ -11,8 +11,8 @@ def read_image(filepath):
 
 	height = eval(raw[1])
 	width = eval(raw[2])
-	print(height)
-	print(width)
+	#print(height)
+	#print(width)
 	temp = []
 	for x in range(height*width):
 		temp.append(eval(raw[x+3]))
