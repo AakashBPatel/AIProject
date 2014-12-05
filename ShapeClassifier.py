953d2d527346202b0f48eaa175c7b7c25c78234b
Aakash Patel <contact@aaka.sh>
2014-12-05 14:46:45 -0500

    more shit

--- ShapeClassifier.py
+++ ShapeClassifier.py
@@ -1,7 +1,7 @@
 import neural
 import shlex, subprocess
 
-SIZE = 10*10
+SIZE = 15*15
 
 def read_image(filepath):
 	print("Reading image: " + filepath)
@@ -52,6 +52,25 @@ def gen_training_data(image_array, input_array):
 	print("Done.")
 	return training_set
 
+def classify(path, net):
+	image_data = [read_image(path)]
+	output = neural.classify_new(net, image_data)
+	summ = 0
+	shape = 0
+	maxx = 0
+	for i in range(len(output)):
+		summ += output[i]
+		if(output[i]>maxx):
+			maxx=output[i]
+			shape=i
+	prop = maxx/summ
+	prop = (prop*100)/100
+	if(shape==0):
+		print("Shape is a Square with %s certainty" % (prop))
+	elif(shape==1):
+		print("Shape is a Plus with %s certainty" % (prop))
+	elif(shape==2):
+		print("Shape is a Square with %s certainty" % (prop))
 
 
 	#output_layer = [neural.u(), neural.u(), neural.u(), neural.u(), neural.u()]
@@ -59,9 +78,13 @@ def gen_training_data(image_array, input_array):
 
 # Make training array and network
 
-image_data = [read_image("/Users/aakash/Downloads/small.png"),read_image("/Users/aakash/Downloads/smallplus.png")]
+image_data = [read_image("/Users/aakash/Downloads/shapeyz/ss.png"),read_image("/Users/aakash/Downloads/shapeyz/sp.png")]
 training_data = gen_training_data(image_data, [[1,0,0,0,0], [0,1,0,0,0]])
 net = neural.buildNet(build_net())
 classified = neural.classify(net, training_data, 0.2,verbose=True)
+#classify("/Users/aakash/Downloads/smallnoise.png", net)
+
+
+
 
 
